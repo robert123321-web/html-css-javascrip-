4dd7cf74e7bae0b47ef6d1099cc7864fe9f71e2b
robert123321-web <roberthaxhiu2012@gmail.com>
2025-01-21 20:48:03 +0100

    day 34

---
+++ day35/main.js
@@ -0,0 +1,16 @@
+// function names(){
+//     document.write("Lorik")
+//     document.write("Bruna")
+//     document.write("Robert")
+//     document.write("Viktoria")
+//     setTimeout(function(){document.write(Patrik);},3000)
+// }
+
+
+// names();
+
+var colors =["blue","red","green","yellow","violet"]
+
+function background(){
+    document.querySelector("body")
+}
