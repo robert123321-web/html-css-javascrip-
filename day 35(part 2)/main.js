db0bf33f86cb499323b65e817e775a4acb12b3db
robert123321-web <roberthaxhiu2012@gmail.com>
2025-01-30 20:44:05 +0100

    day 35part2

---
+++ day 35(part 2)/main.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    var menu = document.getElementById("myLinks")
+    if(menu.style.display==="block"){
+        menu.style.display="none";
+    }
+    else{
+        menu.style.display="block"
+    }
+}
