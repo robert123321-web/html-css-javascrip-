840eb68340961070020cbaa58c82be3da253a2a8
robert123321-web <roberthaxhiu2012@gmail.com>
2025-02-06 20:21:22 +0100

    day36

--- day 36/main.js
+++ day 36/main.js
@@ -1,9 +1,54 @@
-$(window).ready(function(){
-    alert("is working");
+$("li").click(function (){
+    alert("click");
+} );
+
+$("#btn").click(function(){
+    console.log($("h1").text());
+    $("#h1").text("Texting text");
+    $("#h1").append("Extra text");
+});
+
+$("#btn").on("click", function(){
+    console.log("Second button is clicked")
+});
+
+$("#myDiv").click(function(){
+    $(this).addClass("clicked") 
+}).find("span")
+.attr("title" , "hover over me");
+
+
+$("#btn3").click(function(){
+    $(".hidden").show("slow");
+});
+
+
+$("#btn4").click(function(){
+    $(".hidden").hide();
+});
+
+$("#square").click(function(){
+    $("#square").animate({
+        'width':'200px',
+        'height':'200px'
+    })
+
+  
+
+});
+
+$(".testing").click(function(){
+    $(".testing").animate({
+        'left':'+=50px',
+        'opacity':0.25,
+        'fontSize':"30px"
+    }),300,
+
+    function(){
+        console.log("animacioni eshte krijuar")
+
+    }
+
 })
 
-$("li").click(function){
-    console.log
-  $("h1").text("Texting,")
-  $("h1").append("this is a test");
-}
+
