3174c2f828c4adb688a5e1f8a3d2d4503923ecb5
robert123321-web <roberthaxhiu2012@gmail.com>
2025-02-11 20:48:29 +0100

    day38

---
+++ day 38/main.js
@@ -0,0 +1,9 @@
+function displayInfo() {
+    // Get input values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const age = document.getElementById('age').value;
+
+
+    console.log(name)
+}
